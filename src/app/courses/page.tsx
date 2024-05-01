851c902e29ac2086884af1ca85b7e23bf2dc0b3c
Yashshinde43 <yashshinde4343@gmail.com>
2024-05-01 18:42:56 +0530

    Fixed something in the code

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -22,8 +22,8 @@ const page = () => {
         All courses ({courseData.courses.length})
       </h1>
       <div className="flex flex-wrap justify-center">
-        {courseData.courses.map((courses:Course) => (
-          <CardContainer className="inter-var ">
+        {courseData.courses.map((courses: Course) => (
+          <CardContainer key={courses.id} className="inter-var m-4">
             <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
               <CardItem
                 translateZ="50"
